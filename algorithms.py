bed21437aaab93e64cc4f53687d7081bc86a7dec
shonjiho <jihoson94@gmail.com>
2019-09-27 08:55:27 +0900

    refactoring at using constants and import and assigning lambda

--- algorithms.py
+++ algorithms.py
@@ -1,13 +1,12 @@
 import heapq
-
-from constants import *
+import constants
 
 
 def BFS(queue, cells, start, end, delta):
     height, width = len(cells), len(cells[0])
     cell = queue.popleft()
     x, y = cell.pos
-    cell.color = COLOR_LIGHT_BLUE
+    cell.color = constants.COLOR_LIGHT_BLUE
 
     for dx, dy in delta:
         new_x = x + dx
@@ -15,30 +14,30 @@ def BFS(queue, cells, start, end, delta):
 
         if -1 < new_x < width and -1 < new_y < height:
             new_cell = cells[new_y][new_x]
-            if new_cell.color == COLOR_WHITE:
-                new_cell.color = COLOR_LIGHT_GREEN
+            if new_cell.color == constants.COLOR_WHITE:
+                new_cell.color = constants.COLOR_LIGHT_GREEN
                 queue.append(new_cell)
                 new_cell.trace = cell.trace + [(new_x, new_y)]
-            elif new_cell.color == COLOR_RED:
-                status = 'complete'
+            elif new_cell.color == constants.COLOR_RED:
+                status = "complete"
                 path = [start.pos] + cell.trace + [end.pos]
                 return status, path
 
     if not queue:
-        status = 'complete'
+        status = "complete"
         path = []
         return status, path
 
-    status = 'run'
+    status = "run"
     path = []
-    return 'run', path
+    return "run", path
 
 
 def A_star(heap, cells, start, end, delta):
     height, width = len(cells), len(cells[0])
     _, cell = heapq.heappop(heap)
     x, y = cell.pos
-    cell.color = COLOR_LIGHT_BLUE
+    cell.color = constants.COLOR_LIGHT_BLUE
 
     for dx, dy in delta:
         new_x = x + dx
@@ -46,25 +45,25 @@ def A_star(heap, cells, start, end, delta):
 
         if -1 < new_x < width and -1 < new_y < height:
             new_cell = cells[new_y][new_x]
-            if new_cell.color == COLOR_WHITE:
+            if new_cell.color == constants.COLOR_WHITE:
                 if all([(new_x, new_y) != other_cell.pos for _, other_cell in heap]):
-                    new_cell.color = COLOR_LIGHT_GREEN
+                    new_cell.color = constants.COLOR_LIGHT_GREEN
                     new_cell.g = cell.g + 1
                     new_cell.h = (new_x - end.pos[0]) ** 2 + (new_y - end.pos[1]) ** 2
                     new_cell.f = new_cell.g + new_cell.h
                     heapq.heappush(heap, (new_cell.f, new_cell))
                     new_cell.trace = cell.trace + [(new_x, new_y)]
-            elif new_cell.color == COLOR_RED:
-                status = 'complete'
+            elif new_cell.color == constants.COLOR_RED:
+                status = "complete"
                 path = [start.pos] + cell.trace + [end.pos]
                 return status, path
 
     if not heap:
-        status = 'complete'
+        status = "complete"
         path = []
         return status, path
 
-    status = 'run'
+    status = "run"
     path = []
     return status, path
 
@@ -74,7 +73,7 @@ def dijkstra(heap, cells, start, end, delta):
     heapq.heapify(heap)
     _, cell = heapq.heappop(heap)
     x, y = cell.pos
-    cell.color = COLOR_LIGHT_BLUE
+    cell.color = constants.COLOR_LIGHT_BLUE
 
     for dx, dy in delta:
         new_x = x + dx
@@ -82,28 +81,28 @@ def dijkstra(heap, cells, start, end, delta):
 
         if -1 < new_x < width and -1 < new_y < height:
             new_cell = cells[new_y][new_x]
-            if new_cell.color == COLOR_WHITE:
-                new_cell.color = COLOR_LIGHT_GREEN
+            if new_cell.color == constants.COLOR_WHITE:
+                new_cell.color = constants.COLOR_LIGHT_GREEN
                 alt = cell.dist + 1
                 if alt < new_cell.dist:
                     new_cell.dist = alt
                     new_cell.prev = cell
                     heapq.heappush(heap, (new_cell.dist, new_cell))
-            elif new_cell.color == COLOR_RED:
+            elif new_cell.color == constants.COLOR_RED:
                 target = end
                 target.prev = cell
                 path = list()
                 while target:
                     path.append(target.pos)
                     target = target.prev
-                status = 'complete'
+                status = "complete"
                 return status, path
 
     if not heap:
-        status = 'complete'
+        status = "complete"
         path = []
         return status, path
 
-    status = 'run'
+    status = "run"
     path = []
     return status, path
--- pathfinding.py
+++ pathfinding.py
@@ -4,21 +4,21 @@ import time
 from collections import deque
 
 import algorithms
-from constants import *
+import constants
 
 pygame.init()
-pygame.display.set_caption('Pathfinding Simulation')
+pygame.display.set_caption("Pathfinding Simulation")
 
 
 class Cell(object):
     def __init__(self, x_idx, y_idx):
         self.pos = x_idx, y_idx  # 셀의 위치 값(튜플)
-        self.color = COLOR_WHITE  # 셀의 색깔 겸 시작점/끝점/벽/빈공간의 역할
+        self.color = constants.COLOR_WHITE  # 셀의 색깔 겸 시작점/끝점/벽/빈공간의 역할
         self.trace = list()  # 셀의 자취, 최단경로(Simulator.path) 계산할 때 사용
 
     def __lt__(self, other):  # 힙정렬 시 에러 방지용 메서드
         return False
-        
+
     def __repr__(self):  # print 될 때 색 출력
         return str(self.color)
 
@@ -26,21 +26,21 @@ class Cell(object):
 class Simulator(object):
     def __init__(self):
         # 알고리즘 선택 가능 모드
-        self.modes = ['BFS', 'A*', 'Dijkstra']
-        self.mode = DEFAULT_MODE
+        self.modes = ["BFS", "A*", "Dijkstra"]
+        self.mode = constants.DEFAULT_MODE
 
         # 보드 크기
-        self.cell_size = CELL_SIZE
-        self.width_cnt = WIDTH_CNT
-        self.height_cnt = HEIGHT_CNT
+        self.cell_size = constants.CELL_SIZE
+        self.width_cnt = constants.WIDTH_CNT
+        self.height_cnt = constants.HEIGHT_CNT
         self.width_board = self.cell_size * self.width_cnt
         self.height_board = self.cell_size * self.height_cnt
 
         # 패널 크기
-        self.width_panel = WIDTH_PANEL
-        self.margin_panel = MARGIN_PANEL
+        self.width_panel = constants.WIDTH_PANEL
+        self.margin_panel = constants.MARGIN_PANEL
         # 알고리즘 선택 버튼 크기
-        self.mode_size = self.width_panel - 2 * self.margin_panel, MODE_HEIGHT
+        self.mode_size = self.width_panel - 2 * self.margin_panel, constants.MODE_HEIGHT
         self.mode_interval = self.mode_size[1] + self.margin_panel
 
         # 화면 크기
@@ -49,8 +49,13 @@ class Simulator(object):
         self.screen = pygame.display.set_mode((self.width, self.height))
 
         # 셀 초기화
-        self.cells_plane = [[Cell(x_idx, y_idx) for x_idx in range(self.width_cnt)] for y_idx in range(self.height_cnt)]
-        self.cells_flatten = [cell for column in self.cells_plane for cell in column]  # 인덱스 상관없이 모든 요소 순환할 때 요긴함
+        self.cells_plane = [
+            [Cell(x_idx, y_idx) for x_idx in range(self.width_cnt)]
+            for y_idx in range(self.height_cnt)
+        ]
+        self.cells_flatten = [
+            cell for column in self.cells_plane for cell in column
+        ]  # 인덱스 상관없이 모든 요소 순환할 때 요긴함
 
         # 변수 초기화
         self.queue = deque()
@@ -58,57 +63,64 @@ class Simulator(object):
         self.path = list()
         self.start_cell = self.cells_plane[1 - 1][1 - 1]
         self.end_cell = self.cells_plane[self.width_cnt - 1][self.height_cnt - 1]
-        self.delta = WITHOUT_DIAGONAL
-        self.status = 'wait'
+        self.delta = constants.WITHOUT_DIAGONAL
+        self.status = "wait"
 
         # 시간
         self.clock = pygame.time.Clock()
-        self.start_time = 0.
-        self.end_time = 0.
-        self.pause_time = 0.
+        self.start_time = 0.0
+        self.end_time = 0.0
+        self.pause_time = 0.0
 
         # 마우스 / 키 입력
         self.dragging = None
 
     def handle_event(self):
-        '''
+        """
         마우스 / 키보드 입력 받기
-        '''
+        """
         for event in pygame.event.get():
             e_type = event.type
             e_dict = event.dict
 
             # 키보드 F4 도는 우측 상단 X 버튼
-            if e_type == QUIT:
+            if e_type == constants.QUIT:
                 sys.exit()
 
             # 스페이스 바
-            elif e_type == KEYUP and e_dict['key'] == K_SPACE:
-                if self.status == 'wait':  # 대기상태 - 준비상태
-                    self.status = 'ready'
-                elif self.status == 'run':  # 실행 중 - 일시정지
+            elif e_type == constants.KEYUP and e_dict["key"] == constants.K_SPACE:
+                if self.status == "wait":  # 대기상태 - 준비상태
+                    self.status = "ready"
+                elif self.status == "run":  # 실행 중 - 일시정지
                     self.pause_time = time.time()
-                    self.status = 'pause'
-                elif self.status == 'pause':  # 일시정지 - 실행 중
+                    self.status = "pause"
+                elif self.status == "pause":  # 일시정지 - 실행 중
                     self.start_time += time.time() - self.pause_time
-                    self.status = 'run'
-                elif self.status == 'complete':  # 실행 후 - 대기상태 및 초기화
-                    self.status = 'wait'
+                    self.status = "run"
+                elif self.status == "complete":  # 실행 후 - 대기상태 및 초기화
+                    self.status = "wait"
                     for cell in self.cells_flatten:
-                        if cell.color in {COLOR_LIGHT_GREEN, COLOR_LIGHT_BLUE}:
-                            cell.color = COLOR_WHITE
+                        if cell.color in {
+                            constants.COLOR_LIGHT_GREEN,
+                            constants.COLOR_LIGHT_BLUE,
+                        }:
+                            cell.color = constants.COLOR_WHITE
                             cell.trace = list()
                     self.path.clear()
 
             # 대기상태 일 때 ESC
-            elif self.status == 'wait' and e_type == KEYUP and e_dict['key'] == K_ESCAPE:
+            elif (
+                self.status == "wait"
+                and e_type == constants.KEYUP
+                and e_dict["key"] == constants.K_ESCAPE
+            ):
                 for cell in self.cells_flatten:
-                    if cell.color == COLOR_GRAY:  # 벽 없애기
-                        cell.color = COLOR_WHITE
-            
+                    if cell.color == constants.COLOR_GRAY:  # 벽 없애기
+                        cell.color = constants.COLOR_WHITE
+
             # 대기상태 일 때 마우스 입력
-            elif self.status == 'wait' and e_type in {MOUSEBUTTONDOWN, MOUSEMOTION, MOUSEBUTTONUP}:
-                x, y = e_dict['pos']
+            elif self.status == "wait" and e_type in constants.MOUSE_EVENTS:
+                x, y = e_dict["pos"]
                 x_idx, y_idx = x // self.cell_size, y // self.cell_size
 
                 # 마우스가 보드 안
@@ -116,47 +128,50 @@ class Simulator(object):
                     cell = self.cells_plane[y_idx][x_idx]
 
                     # 마우스 좌클릭
-                    if e_type == MOUSEBUTTONDOWN and e_dict['button'] == 1:
+                    if e_type == constants.MOUSE_BUTTON_DOWN and e_dict["button"] == 1:
 
                         # 클릭 / 드래그 동안의 상태 self.dragging에 저장
-                        if not self.dragging: 
-                            if cell.color == COLOR_WHITE:
-                                self.dragging = 'create wall'
-                            elif cell.color == COLOR_GRAY:
-                                self.dragging = 'remove wall'
-                            elif cell.color == COLOR_GREEN:
-                                self.dragging = 'move start'
-                            elif cell.color == COLOR_RED:
-                                self.dragging = 'move end'
+                        if not self.dragging:
+                            if cell.color == constants.COLOR_WHITE:
+                                self.dragging = "create wall"
+                            elif cell.color == constants.COLOR_GRAY:
+                                self.dragging = "remove wall"
+                            elif cell.color == constants.COLOR_GREEN:
+                                self.dragging = "move start"
+                            elif cell.color == constants.COLOR_RED:
+                                self.dragging = "move end"
 
                         # 벽 / 빈 공간 클릭 시 토글
-                        if self.dragging == 'create wall':
-                            cell.color = COLOR_GRAY
-                        elif self.dragging == 'remove wall':
-                            cell.color = COLOR_WHITE
+                        if self.dragging == "create wall":
+                            cell.color = constants.COLOR_GRAY
+                        elif self.dragging == "remove wall":
+                            cell.color = constants.COLOR_WHITE
 
                     # 마우스 드래그(좌클릭)
-                    elif e_type == MOUSEMOTION and e_dict['buttons'] == LEFT_CLICK:
+                    elif (
+                        e_type == constants.MOUSE_MOTION
+                        and e_dict["buttons"] == constants.LEFT_CLICK
+                    ):
                         # 벽 / 빈 공간 드래그 시 토글
-                        if self.dragging == 'create wall':
-                            cell.color = COLOR_GRAY
-                        elif self.dragging == 'remove wall':
-                            cell.color = COLOR_WHITE
+                        if self.dragging == "create wall":
+                            cell.color = constants.COLOR_GRAY
+                        elif self.dragging == "remove wall":
+                            cell.color = constants.COLOR_WHITE
                         # 시작점 / 끝 점 드래그 시 이동
-                        elif self.dragging == 'move start':
-                            self.start_cell.color = COLOR_WHITE
+                        elif self.dragging == "move start":
+                            self.start_cell.color = constants.COLOR_WHITE
                             self.start_cell = self.cells_plane[y_idx][x_idx]
-                        elif self.dragging == 'move end':
-                            self.end_cell.color = COLOR_WHITE
+                        elif self.dragging == "move end":
+                            self.end_cell.color = constants.COLOR_WHITE
                             self.end_cell = self.cells_plane[y_idx][x_idx]
 
-                    elif e_type == MOUSEBUTTONUP:
+                    elif e_type == constants.MOUSE_BUTTON_UP:
                         self.dragging = None
 
                 # 마우스가 패널 안
                 elif x >= self.width_board:
                     # 마우스 좌클릭
-                    if e_type == MOUSEBUTTONDOWN and e_dict['button'] == 1:
+                    if e_type == constants.MOUSE_BUTTON_DOWN and e_dict["button"] == 1:
                         for num, mode in enumerate(self.modes):
 
                             mode_left = self.width_board + self.margin_panel
@@ -165,131 +180,166 @@ class Simulator(object):
                             mode_bottom = (num + 1) * self.mode_interval
 
                             # 알고리즘 선택 버튼 안
-                            if mode_left < x < mode_right and mode_top < y < mode_bottom:
+                            if (
+                                mode_left < x < mode_right
+                                and mode_top < y < mode_bottom
+                            ):
                                 self.mode = mode  # 모드 설정
-                                print('Mode: {}'.format(mode))
+                                print("Mode: {}".format(mode))
                                 break
 
     def ready(self):
-        '''
+        """
         알고리즘 실행 전 설정
-        '''
-        if self.mode == 'BFS':
+        """
+        if self.mode == "BFS":
             self.queue.append(self.start_cell)
-            
-        elif self.mode == 'A*':
+
+        elif self.mode == "A*":
             for cell in self.cells_flatten:
-                cell.g, cell.h, cell.f = 0, 0, 0  #  A* 알고리즘을 위한 g, h, f 값 초기화
+                #  A* 알고리즘을 위한 g, h, f 값 초기화
+                cell.g, cell.h, cell.f = 0, 0, 0
             self.heap.append((self.start_cell.g, self.start_cell))
 
-        elif self.mode == 'Dijkstra':
+        elif self.mode == "Dijkstra":
             for cell in self.cells_flatten:
-                cell.dist = 0 if cell == self.start_cell else float('inf')  # Dijkstra 알고리즘을 위한 dist 값 초기화
-                cell.prev = None                                            # Dijkstra 알고리즘을 위한 prev 값 초기화
+                cell.dist = (
+                    0 if cell == self.start_cell else float("inf")
+                )  # Dijkstra 알고리즘을 위한 dist 값 초기화
+                cell.prev = None  # Dijkstra 알고리즘을 위한 prev 값 초기화
             self.heap.append((self.start_cell.dist, self.start_cell))
 
-        self.status = 'run'
+        self.status = "run"
         self.start_time = time.time()  # 시간 측정 시작
 
     def run(self):
-        '''
+        """
         알고리즘 실행
-        '''
-        if self.mode == 'BFS':
-            args = self.queue, self.cells_plane, self.start_cell, self.end_cell, self.delta
+        """
+        if self.mode == "BFS":
+            args = (
+                self.queue,
+                self.cells_plane,
+                self.start_cell,
+                self.end_cell,
+                self.delta,
+            )
             self.status, self.path = algorithms.BFS(*args)
-            
-        elif self.mode == 'A*':
-            args = self.heap, self.cells_plane, self.start_cell, self.end_cell, self.delta
+
+        elif self.mode == "A*":
+            args = (
+                self.heap,
+                self.cells_plane,
+                self.start_cell,
+                self.end_cell,
+                self.delta,
+            )
             self.status, self.path = algorithms.A_star(*args)
 
-        elif self.mode == 'Dijkstra':
-            args = self.heap, self.cells_plane, self.start_cell, self.end_cell, self.delta
-            self.status, self.path = algorithms.dijkstra(*args) 
+        elif self.mode == "Dijkstra":
+            args = (
+                self.heap,
+                self.cells_plane,
+                self.start_cell,
+                self.end_cell,
+                self.delta,
+            )
+            self.status, self.path = algorithms.dijkstra(*args)
+
+    def resize_idx_to_cell(self, idx):
+        return self.cell_size * idx + self.cell_size // 2
 
     def complete(self):
-        '''
+        """
         알고리즘 실행 후 처리
-        '''
+        """
         if self.start_time:  # 알고리즘 실행 끝난 후 한 번만 통과
             self.end_time = time.time()  # 시간 측정 종료
-            print('Time: {:.2f} s'.format(self.end_time - self.start_time))
-            self.start_time = 0.
-            self.end_time = 0.
+            print("Time: {:.2f} s".format(self.end_time - self.start_time))
+            self.start_time = 0.0
+            self.end_time = 0.0
 
             self.queue.clear()  # 자료구조 초기화
             self.heap.clear()  # 자료구조 초기화
 
             # self.path의 인덱스 값을 실제 픽셀단위 위치로 변환
-            idx_to_len = lambda idx: self.cell_size * idx + self.cell_size // 2
-            self.path = [tuple(map(idx_to_len, (idx_x, idx_y))) for idx_x, idx_y in self.path]
+            self.path = [
+                tuple(map(self.resize_index_to_cell, (idx_x, idx_y)))
+                for idx_x, idx_y in self.path
+            ]
 
     def draw(self):
-        '''
+        """
         화면에 띄우기
-        '''
+        """
         # 시작점 / 끝점 칠하기
-        self.start_cell.color = COLOR_GREEN
-        self.end_cell.color = COLOR_RED
+        self.start_cell.color = constants.COLOR_GREEN
+        self.end_cell.color = constants.COLOR_RED
 
         # 배경 칠하기
-        self.screen.fill(COLOR_BLACK)
+        self.screen.fill(constants.COLOR_BLACK)
 
         # 각 셀들 흰 색으로 칠하기
         rect_size = self.cell_size - 1
         for y_idx, row in enumerate(self.cells_plane):
             for x_idx, cell in enumerate(row):
                 rect_pos = tuple(self.cell_size * idx for idx in (x_idx, y_idx))
-                rect = pygame.Rect(*rect_pos, rect_size, rect_size) 
+                rect = pygame.Rect(*rect_pos, rect_size, rect_size)
                 pygame.draw.rect(self.screen, cell.color, rect)
 
         # 최단 경로선 칠하기
-        if self.status == 'complete':
+        if self.status == "complete":
             if self.path and len(self.path) > 1:
-                pygame.draw.lines(self.screen, COLOR_YELLOW, False, self.path, 2)
+                pygame.draw.lines(
+                    self.screen, constants.COLOR_YELLOW, False, self.path, 2
+                )
 
         # 알고리즘 선택버튼 상자 칠하기 / 텍스트 설정
-        font = pygame.font.Font(PATH_FONT, 20)
+        font = pygame.font.Font(constants.PATH_FONT, 20)
         for num, mode in enumerate(self.modes):
             # 상자 칠하기
             rect_left = self.width_board + self.margin_panel
             rect_top = self.margin_panel + num * (self.mode_size[1] + self.margin_panel)
             rect = pygame.Rect(rect_left, rect_top, *self.mode_size)
-            pygame.draw.rect(self.screen, COLOR_GRAY, rect)
+            pygame.draw.rect(self.screen, constants.COLOR_GRAY, rect)
             if mode == self.mode:
-                pygame.draw.rect(self.screen, COLOR_ORANGE, rect, 2)
+                pygame.draw.rect(self.screen, constants.COLOR_ORANGE, rect, 2)
 
             # 텍스트 설정
-            font_surface = font.render(mode, True, COLOR_BLACK)
+            font_surface = font.render(mode, True, constants.COLOR_BLACK)
             font_size = font_surface.get_size()
             font_left = self.width_board + (self.width_panel - font_size[0]) / 2
-            font_top = self.margin_panel + num * (self.mode_size[1] + self.margin_panel) + (self.mode_size[1] - font_size[1]) / 2
-            self.screen.blit(font_surface, (font_left,font_top))
+            font_top = (
+                self.margin_panel
+                + num * (self.mode_size[1] + self.margin_panel)
+                + (self.mode_size[1] - font_size[1]) / 2
+            )
+            self.screen.blit(font_surface, (font_left, font_top))
 
         # 화면에 띄우기
         pygame.display.flip()
         # pygame.display.update()
 
     def exec(self):
-        '''
+        """
         시뮬레이터 실행
-        '''
+        """
         while True:
             self.handle_event()  # 마우스 / 키보드 입력 받기
 
-            if self.status == 'ready':  # 알고리즘 실행 전 설정
+            if self.status == "ready":  # 알고리즘 실행 전 설정
                 self.ready()
-            if self.status == 'run':  # 알고리즘 실행
+            if self.status == "run":  # 알고리즘 실행
                 self.run()
-            if self.status == 'complete':  # 알고리즘 실행 후 처리
+            if self.status == "complete":  # 알고리즘 실행 후 처리
                 self.complete()
 
             self.draw()  # 화면에 띄우기
 
-            self.clock.tick(FPS)  # FPS 일정하게 조절
+            self.clock.tick(constants.FPS)  # FPS 일정하게 조절
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     simulator = Simulator()
 
     simulator.exec()  # 시뮬레이터 실행
